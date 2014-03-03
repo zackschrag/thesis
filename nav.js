015d62b711c0f309b32d9a07860737b7f3e88474
Zack Schrag <zackschrag@yahoo.com>
2014-03-03 16:50:02 -0700

    Edited messages.html a bit

--- nav.js
+++ nav.js
@@ -1,10 +1,13 @@
 $(document).ready( function() {
-	$('#page-nav-item-home').click( function() {
+	/*$('#page-nav-item-home').click( function() {
 		$('#page-container-home').show();
 	});
 
 	$('#page-nav-item-messages').click( function() {
 		$('#page-container-home').hide();
 		$('#page-container-messages').show();
+	});*/
+	$('.checkbox').click( function(evt) {
+		//evt.preventDefault();
 	});
 });
