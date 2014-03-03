2cb7b9bfbce9cd83dfcefa0aed34c4849292e817
Zack Schrag <zackschrag@yahoo.com>
2014-03-03 13:45:06 -0700

    Started getting design for home page

---
+++ nav.js
@@ -0,0 +1,10 @@
+$(document).ready( function() {
+	$('#page-nav-item-home').click( function() {
+		$('#page-container-home').show();
+	});
+
+	$('#page-nav-item-messages').click( function() {
+		$('#page-container-home').hide();
+		$('#page-container-messages').show();
+	});
+});
